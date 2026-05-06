8cc7ed6b71a124942290559e63c8309f2df97058
Mohd Ammar <mohd.ammar@squareboat.com>
2026-05-06 11:12:00 +0530

    ADD: create and display a new task

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"github.com/ammarkhan575/task-manager/internal/task"
+)
 
 func main() {
 	fmt.Println("Task Manager v0.1")
+	t := task.NewTask(
+		1,
+		"Finish Go project",
+		"Complete the task manager project in Go",
+		task.PriorityHigh,
+		time.Now().Add(48*time.Hour),
+		[]string{"go", "project"},
+	)
+	fmt.Printf("Task created: %+v\n", t)
 }
